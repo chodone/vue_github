18449a5c5237e91919b211d407f28e41aff78bd4
chodone <saeloun8797@gmail.com>
2022-11-14 17:57:36 +0900

    vue - django

---
+++ exercise/09_ex/09_ex/vue_07_workshop/client/src/views/todos/CreateTodo.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <input 
+      type="text" 
+      v-model.trim="title" 
+      @keyup.enter="createTodo"
+    >
+    <button @click="createTodo">+</button>
+  </div>
+</template>
+
+<script>
+import axios from'axios'
+
+const API_URL = 'http://127.0.0.1:8000'
+
+export default {
+  name: 'CreateTodo',
+  data: function () {
+    return {
+      title: null,
+    }
+  },
+  methods: {
+    createTodo: function () {
+      axios({
+        method: 'post',
+        url: `${API_URL}/todos/`,
+        data:{
+          title: this.title,
+          is_completed: false
+        }
+      })
+        .then((res) => {
+          console.log(res)
+          this.$router.push({name : 'TodoList' })
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+  }
+}
+</script>
---
+++ exercise/09_ex/홈워크_vue_07_hw02_P/CreateTodo.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <input 
+      type="text" 
+      v-model="title" 
+      @keyup.enter="createTodo"
+    >
+    <button @click="createTodo">+</button>
+  </div>
+</template>
+
+<script>
+import axios from'axios'
+
+const API_URL = 'http://127.0.0.1:8000'
+
+export default {
+  name: 'CreateTodo',
+  data: function () {
+    return {
+      title: null,
+    }
+  },
+  methods: {
+    createTodo: function () {
+      axios({
+        methods:'post',
+        url: `${API_URL}/todos/`,
+        data:{
+          title: this.title,
+          is_completed: false
+        }
+      })
+        .then((res) => {
+          console.log(res)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+  },
+}
+</script>
